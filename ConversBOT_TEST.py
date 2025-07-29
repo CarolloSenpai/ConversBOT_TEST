673086bc016c1c12c2a3d2700391443d9aa78fba
CarolloSen <134587331+CarolloSenpai@users.noreply.github.com>
2025-07-29 21:57:17 +0200

    Update ConversBOT_TEST.py
    630 -->600

--- ConversBOT_TEST.py
+++ ConversBOT_TEST.py
@@ -827,7 +827,7 @@ def main():
         st.markdown("""
         <div style="overflow-x: auto; width: 100%;">
           <table style="
-              min-width: 630px;
+              min-width: 600px;
               width: 100%;
               border-collapse: collapse;
               margin: 1rem auto;
